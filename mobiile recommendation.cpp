0b8800b160e9566bebee0370b17141af35383584
abdallanabiel <130190603+abdallanabiel@users.noreply.github.com>
2023-04-13 21:13:52 +0200

    Add files via upload

---
+++ mobiile recommendation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "mobiile_brands.h"
+#include "Samsung.h"
+using namespace std;
+
+
+int main() {
+	int x;
+//	cout << " Enter your budget \n 1. Less than 1500\n2. 1500 : 3000\n3. 3000 : 4500\n4. Above 4500\n";
+
+cout << "do you want to find your mobile by \n1.price \n2. brand " << endl;
+cin >> x ;
+switch  (x)
+{
+	    case (1):
+	    	
+		mobiile_brands o1;
+		
+	   // break;
+		
+		//case (2):
+		//Search o;	
+}
+//	mobiile_brands o1;
+	
+
+
+
+
+
+
+
+
+	return 0;
+}
